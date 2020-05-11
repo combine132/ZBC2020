b042145836f0dfe0b2b4dfec90b9e0b478afc488
combine132 <simonmadsen132@gmail.com>
2020-05-11 13:23:58 +0200

    Opgave 2

---
+++ Opgave 2/Opgave 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            Random dice = new Random();
+            int rollDice = dice.Next(1,7);
+            Console.WriteLine("du slog en " + rollDice+"'er");
+            Console.ReadLine();
+        }
+    }
+}
