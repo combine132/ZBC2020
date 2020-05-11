6ca462e5de7f4d016762dc136f854092c8beca46
BuildTools <unconfigured@null.spigotmc.org>
2020-05-11 13:14:07 +0200

    Opgave 1

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal1 = 16;
+            double tal2 = 5.1;
+
+            double result = tal1 + tal2;
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
