adc9ca00ed8bdf5af704ea3f1c87ced18aa4168a
combine132 <simonmadsen132@gmail.com>
2020-05-11 13:48:18 +0200

    Opgave 3

---
+++ Opgave 3 - Løkker/Opgave 3 - Løkker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave_3___Løkker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ---For Loop---
+
+            //for (int i = 100; i > 1; i--) //Loop 0-99. 100 tal
+            //{
+            //    if (i > 50) //Stopper loop ved 49. 50 tal
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            // ---While Loop---
+
+            //int i = 0;
+            //while (i < 100)
+            //{
+            //    if (i<50)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            // ---For Loop 2 tabel---
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("{0} ", i * 2);
+            }
+        }
+    }
+}
