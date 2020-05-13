4ec143a8ac612a3a7425ea756e68b688120da649
combine132 <simonmadsen132@gmail.com>
2020-05-13 15:00:39 +0200

    Methods

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,59 @@ namespace Methods
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Calculator v0.0.0.1\n+ Addition\n- Subtraction\n* Multiplication\n/ Division");
+            string math = Console.ReadLine();
+            Console.WriteLine($"\nInput numbers");
+            string inpOne = Console.ReadLine();
+            string inpTwo = Console.ReadLine();
+
+            double NumOne = Convert.ToInt32(inpOne);
+            double NumTwo = Convert.ToInt32(inpTwo);
             
+            if (math == "+")
+            {
+                double result = Add(NumOne, NumTwo);
+                Console.WriteLine(result);
+            }
+            else if (math == "-")
+            {
+                double result = Subs(NumOne, NumTwo);
+                Console.WriteLine(result);
+            }
+            else if (math == "*")
+            {
+                double result = Mult(NumOne, NumTwo);
+                Console.WriteLine(result);
+            }
+            else if (math == "/")
+            {
+                double result = Div(NumOne, NumTwo);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine($"Error\nPlease enter 1-4 only.");
+            }
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subs(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Mult(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Div(double a, double b)
+        {
+            return a / b;
         }
     }
 }
